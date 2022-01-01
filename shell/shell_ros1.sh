02c3ace98f09e2e7d5f7d56e3e99fe31470074e3
programmatoroSeduto <francesc.ganci@gmail.com>
2022-01-01 11:47:45 +0000

    shell scripts

--- shell/shell_ros1.sh
+++ shell/shell_ros1.sh
@@ -3,4 +3,4 @@
 source /opt/ros/noetic/setup.bash
 source /root/simple_bridge/ws_ros1/devel/setup.bash
 
-rosrun ros1_listener_pkg ros1_listener
+roslaunch my_custom_listener run_single_topic_test.launch 
--- shell/shell_ros2.sh
+++ shell/shell_ros2.sh
@@ -3,4 +3,4 @@
 source /opt/ros/foxy/setup.bash
 source /root/simple_bridge/ws_ros2/install/setup.sh
 
-ros2 run ros2_publisher_pkg ros2_publisher
+ros2 launch my_custom_sender run_single_topic_test.py
