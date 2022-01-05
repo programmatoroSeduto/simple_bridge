5669e2d25f57c160d867d38db388bdf1cea8b55d
programmatoroSeduto <francesc.ganci@gmail.com>
2022-01-05 22:31:13 +0000

    removed useless files

--- shell/shell_ros1_listener.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-source /opt/ros/noetic/setup.bash
-source /root/simple_bridge/ws_ros1/devel/setup.bash
-
-rosrun ros1_listener_pkg ros1_listener
--- shell/shell_ros2_publisher.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-source /opt/ros/foxy/setup.bash
-source /root/simple_bridge/ws_ros2/install/setup.sh
-
-ros2 run ros2_publisher_pkg ros2_publisher
